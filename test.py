a2a75c43a1fe741104b8f04b50d59db996e0ef6f
chung <94212671+Yi-Chung-Chen@users.noreply.github.com>
2024-06-12 16:44:08 +0800

    test.py

--- test.py
+++ test.py
@@ -1,70 +1,84 @@
+"""Module providing a function time counting."""
+
 import time
 
 
 class EdgeComputing():
-    def __init__(self, R: float, W: float, np: float, C: float, wl: float):
-        self.TranSpeed = R * 10**3
-        self.Bandwidth = W * 10**6
-        self.NoisePower = np
-        self.ServiceSize = C * 10**3
-        self.WaveLength = wl
-        self.ts = C/R
+    """"class representing EdgeComputing"""
 
-    def SNR(self) -> float:
-        ratio = self.TranSpeed/self.Bandwidth
-        snr = 2**ratio - 1 
+    def __init__(self, transmission_speed: float, bandwidth: float,
+                 noise_power: float, service_size: float, wave_length: float):
+        self.transmission_speed = transmission_speed * 10**3
+        self.bandwidth = bandwidth * 10**6
+        self.noise_power = noise_power
+        self.service_size = service_size * 10**3
+        self.wave_length = wave_length
+        self.communication_time = service_size/bandwidth
+
+    def signal_noise_ratio(self) -> float:
+        """funtion to calculate signal-noise ratio"""
+
+        ratio = self.transmission_speed/self.bandwidth
+        snr = 2**ratio-1
         return snr
 
-    def Power(self, d) -> float:
-        snr = self.SNR()
-        power = self.NoisePower*snr*(d**2)/(self.WaveLength**2)
+    def power(self, distance: float) -> float:
+        """function to calculate power"""
+
+        snr = self.signal_noise_ratio()
+        power = self.noise_power*snr*(distance**2)/(self.wave_length**2)
         return power
 
-    def migration_energy_consumption(self, d) -> float:
-        Pm = self.Power(d) # distance between targe and source
-        Emc = self.ServiceSize*Pm/self.TranSpeed
-        return Emc
+    def migration_energy_consumption(self, distance_between_target_and_source: float) -> float:
+        """function to calculate migration_energy_consumption"""
 
-    def communication_energy_consumption(self, d) -> float:
-        Pc = self.Power(d) # distancce between user and source
-        Ecc = Pc*self.ts  # 暫時只考慮tranmission delay，實際上要考慮 transmission/processing/queue delay
-        return Ecc
+        power_of_migration = self.power(distance_between_target_and_source)
+        energy_of_migration_consumption = self.service_size*power_of_migration/self.transmission_speed
+        return energy_of_migration_consumption
 
-ue_speed = 14 # m/s -> 100 km/hr
-total_energy_consumption = 0
+    def communication_energy_consumption(self, distacne_between_source_and_ue: float) -> float:
+        """function to calculate communication_energy_consumption"""
+
+        power_of_communication = self.power(distacne_between_source_and_ue)
+        energy_of_communication_consumption = power_of_communication*self.communication_time
+        # 暫時只考慮tranmission delay，實際上要考慮 transmission/processing/queue delay
+        return energy_of_communication_consumption
+
+UE_SPEED = 14 # m/s -> 100 km/hr
+TOTAL_CONSUMPTION = 0
 
 # --- bs1 ----500---- bs2 ----500---- bs3
-server_position = [0, 250, 500,750,1000]
+SERVER_POSITION = [0, 250, 500, 750, 1000]
 
-i = 0
-serverNo = 0
-ec = EdgeComputing(R=200, W=5, np=3, C=1000, wl=0.125)
-do_migration = False
-mode_on = False
-while ue_speed*i<server_position[-1]:
-    if not mode_on:
-        if ue_speed*i>=250 and do_migration==False: # service migration from bs1 to bs2
-            do_migration = True
-            total_energy_consumption += ec.migration_energy_consumption(server_position[1]-server_position[0])
-            serverNo = 1
+SECOND = 0
+SERVER_NO = 0
+EC = EdgeComputing(transmission_speed=200, bandwidth=5, 
+                   noise_power=3, service_size=1000, wave_length=0.125)
+
+DO_MIGRATION = False
+MODE_ON = False
+
+while UE_SPEED*SECOND<SERVER_POSITION[-1]:
+    if not MODE_ON:
+        if UE_SPEED*SECOND>=250 and DO_MIGRATION is False: # service migration from bs1 to bs2
+            DO_MIGRATION = True
+            TOTAL_CONSUMPTION += EC.migration_energy_consumption(SERVER_POSITION[1]-SERVER_POSITION[0])
+            SERVER_NO = 1
         
-        if ue_speed*i>=750 and do_migration == True: # service migration from bs2 to bs3
-            do_migration = False
-            total_energy_consumption += ec.migration_energy_consumption(server_position[-1]-server_position[1])
-            serverNo = 2
+        if UE_SPEED*SECOND>=750 and DO_MIGRATION is True: # service migration from bs2 to bs3
+            DO_MIGRATION = False
+            TOTAL_CONSUMPTION += EC.migration_energy_consumption(SERVER_POSITION[-1]-SERVER_POSITION[1])
+            SERVER_NO = 2
     else:
-        if ue_speed*i>=750 and do_migration == False:
-            do_migration = True
-            total_energy_consumption +=ec.migration_energy_consumption(1000)
-            serverNo = 2
+        if UE_SPEED*SECOND>=750 and DO_MIGRATION is False:
+            DO_MIGRATION = True
+            TOTAL_CONSUMPTION +=EC.migration_energy_consumption(1000)
+            SERVER_NO = 2
 
-    total_energy_consumption += (ec.communication_energy_consumption(abs(ue_speed*i-server_position[serverNo]))/2)
+    TOTAL_CONSUMPTION += (EC.communication_energy_consumption(abs(UE_SPEED*SECOND-SERVER_POSITION[SERVER_NO]))/2)
     
-    print(f'當前服務基站bs{serverNo+1}')
-    print(f'ue位置:{ue_speed*i}，與bs{serverNo+1}的距離:{abs(ue_speed*i-server_position[serverNo])}')
-    print(f'當前總能耗:{total_energy_consumption:.2f}')
-    
-    i+=1
-    time.sleep(1)
-
-
+    print(f'當前服務基站bs{SERVER_NO+1}')
+    print(f'ue位置:{UE_SPEED*SECOND}，與bs{SERVER_NO+1}的距離:{abs(UE_SPEED*SECOND-SERVER_POSITION[SERVER_NO])}')
+    print(f'當前總能耗:{TOTAL_CONSUMPTION:.2f}')
+    SECOND+=1
+    time.sleep(1.0)
