e8857cdae645ce0d87c07d2473936495e78043b2
chung <94212671+Yi-Chung-Chen@users.noreply.github.com>
2024-06-12 15:12:52 +0800

    test.py
    2024/6/26 顯示兩者的比較

--- test.py
+++ test.py
@@ -30,34 +30,39 @@ class EdgeComputing():
         Ecc = Pc*self.ts  # 暫時只考慮tranmission delay，實際上要考慮 transmission/processing/queue delay
         return Ecc
 
-ue_speed = 28 # m/s -> 100 km/hr
+ue_speed = 14 # m/s -> 100 km/hr
 total_energy_consumption = 0
 
 # --- bs1 ----500---- bs2 ----500---- bs3
-server_position = [0, 500, 1000]
+server_position = [0, 250, 500,750,1000]
 
 i = 0
 serverNo = 0
-ec = EdgeComputing(R=200, W=5, np=3, C=1000, wl=0.115)
+ec = EdgeComputing(R=200, W=5, np=3, C=1000, wl=0.125)
 do_migration = False
+mode_on = False
 while ue_speed*i<server_position[-1]:
+    if not mode_on:
+        if ue_speed*i>=250 and do_migration==False: # service migration from bs1 to bs2
+            do_migration = True
+            total_energy_consumption += ec.migration_energy_consumption(server_position[1]-server_position[0])
+            serverNo = 1
+        
+        if ue_speed*i>=750 and do_migration == True: # service migration from bs2 to bs3
+            do_migration = False
+            total_energy_consumption += ec.migration_energy_consumption(server_position[-1]-server_position[1])
+            serverNo = 2
+    else:
+        if ue_speed*i>=750 and do_migration == False:
+            do_migration = True
+            total_energy_consumption +=ec.migration_energy_consumption(1000)
+            serverNo = 2
 
-    print(ec.SNR())
-    if ue_speed*i>=250 and do_migration==False: # service migration from bs1 to bs2
-        do_migration = True
-        total_energy_consumption += ec.migration_energy_consumption(server_position[1]-server_position[0])
-        serverNo = 1
-    
-    if ue_speed*i>=750 and do_migration == True: # service migration from bs2 to bs3
-        do_migration = False
-        total_energy_consumption += ec.migration_energy_consumption(server_position[-1]-server_position[1])
-        serverNo = 2
-
-    total_energy_consumption += ec.communication_energy_consumption(abs(ue_speed*i-server_position[serverNo]))
+    total_energy_consumption += (ec.communication_energy_consumption(abs(ue_speed*i-server_position[serverNo]))/2)
     
     print(f'當前服務基站bs{serverNo+1}')
     print(f'ue位置:{ue_speed*i}，與bs{serverNo+1}的距離:{abs(ue_speed*i-server_position[serverNo])}')
-    print(f'當前總能耗:{total_energy_consumption}')
+    print(f'當前總能耗:{total_energy_consumption:.2f}')
     
     i+=1
     time.sleep(1)
